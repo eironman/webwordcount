9bc88d71dd3b5e392d91bb2dae7a76a7c79042c1
aaron <aaron.amengual@gmail.com>
2014-03-19 13:50:26 +0100

    Export to csv

--- public/js/main.js
+++ public/js/main.js
@@ -11,5 +11,30 @@ $(function(){
 		$("#form-count").submit();
 	})
 	
+	// Export to csv
+	$("#csv").click(function(){
+		
+		var data = 'data:text/csv;charset=utf-8,';
+		var words = [];
+		
+		// Headings
+		$("#result th").each(function(){
+			words.push($(this).html().replace(" ", "_"));
+		});
+		data += words.join(";") + "\n";
+		
+		// Words
+		$("#result tr").each(function(){
+			words = [];
+			$(this).children("td").each(function(){
+				words.push($(this).html());				
+			});
+			data += words.join(";") + "\n";
+		});
+		
+		var encodedUri = encodeURI(data);
+		window.open(encodedUri);
+	})
+	
 	var form = new FormView();
 });
